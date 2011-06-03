bcc2a1ec3ff8592860c206e767866aa195c61198
rackom <rackom10@gmail.com>
2011-06-03 13:17:55 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IMesto::Application.config.secret_token = 'a1bca43cbf5a3017a8c8a4fbcad296cdfe303244cd209030d4352a62cfdccb946cec3e85ea7ac1eefc686ae21c4b147c1c86418ea4aee62339d42d54216c4136'
