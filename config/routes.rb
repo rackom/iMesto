e7d8ac13777c5642c766ca1af4eff697c5b8abc1
rackom <rackom10@gmail.com>
2011-06-03 13:41:35 +0200

    upraveny gemfile + routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 IMesto::Application.routes.draw do
   resources :messages
+  
+  root :to => 'messages#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
