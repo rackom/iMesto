db5b80a4057076e813f7824f6e4e1a593ebe9545
rackom <rackom10@gmail.com>
2011-06-13 16:48:43 +0200

    pridane url do response v.2

--- vendor/plugins/tequila/init.rb
+++
@@ -1,3 +0,0 @@
-require 'ostruct'
-require 'treetop'
-require 'tequila_jazz_handler'
--- vendor/plugins/tequila/test/test_helper.rb
+++
@@ -1,5 +0,0 @@
-require 'rubygems'
-require 'active_record'
-require 'ostruct'
-require File.dirname(__FILE__) + '/../lib/tequila'
-require File.dirname(__FILE__) + '/db/preparing'
